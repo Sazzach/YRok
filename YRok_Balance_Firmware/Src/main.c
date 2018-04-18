e2fb2e62a3fcfd8dc8b836363ddf00b93959c9ef
Michael Zhang <michael13162@gmail.com>
2018-04-17 21:06:05 -0600

    Got uart echo working

--- YRok_Balance_Firmware/Src/main.c
+++ YRok_Balance_Firmware/Src/main.c
@@ -61,6 +61,66 @@ void SystemClock_Config(void);
 
 /* USER CODE BEGIN 0 */
 
+void transmit_char(char c) {
+  // wait until transmit data register is empty
+  while (!(USART4->ISR & (1 << 7))) {
+  }
+
+  // write c to transmit data register
+  USART4->TDR = c;
+}
+
+void transmit_string(char *s) {
+  while (*s != 0) {
+    transmit_char(*s);
+    s++;
+  }
+}
+
+char buffer[7];
+int idx = 0;
+volatile int data = 0;
+
+void USART3_4_IRQHandler(void) {
+  buffer[idx] = USART4->RDR;
+  if (buffer[idx] == '\n' || buffer[idx] == '\r') {
+    idx = 0;
+    data = 1;
+  } else {
+    idx++;
+  }
+}
+
+void init_uart() {
+  RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+  // set up Rx(PA1)/Tx(PA0) - USART4
+  RCC->APB1ENR |= RCC_APB1ENR_USART4EN;
+
+  // alternate function mode
+  GPIOA->MODER &= ~((0x1 << 2*0) | (0x1 << 2*1));
+  GPIOA->MODER |= (0x2 << 2*0) | (0x2 << 2*1);
+  
+  // alternate function 4
+  GPIOA->AFR[0] &= ~(0xb << 4*0);
+  GPIOA->AFR[0] |= 0x4 << 4*0;
+  GPIOA->AFR[0] &= ~(0xb << 4*1);
+  GPIOA->AFR[0] |= 0x4 << 4*1;
+
+  // configure baud rate
+  USART4->BRR = HAL_RCC_GetHCLKFreq() / 115200;
+
+  // enable receiver/transmitter hardware
+  USART4->CR1 |= (1 << 2) | (1 << 3) | USART_CR1_RXNEIE;  // RXNEIE causes interrupt to be generated
+
+  // enable USART
+  USART4->CR1 |= 1 << 0;
+
+  // set up NVIC
+  NVIC_EnableIRQ(USART3_4_IRQn);
+  NVIC_SetPriority(USART3_4_IRQn, 0);
+}
+
 void init_usr_led()
 {
   RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
@@ -77,11 +137,19 @@ int main(void)
   SystemClock_Config();
 
   init_usr_led();
+  init_uart();
+  transmit_string("Repeat: ");
 
   while (1)
   {
   	HAL_Delay(1000);
-	GPIOC->ODR ^= (0x1 << 15);
+    GPIOC->ODR ^= (0x1 << 15);
+
+    // echo over uart
+    while (!data) {
+    }
+    transmit_string(buffer);
+    data = 0;
   }
 }
 
